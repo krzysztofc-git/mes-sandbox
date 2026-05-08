b588d32fe63ce677d1ce1d5f546793dc49965ae6
Krzysztof <118632157+krzysztofc-git@users.noreply.github.com>
2026-05-08 08:21:40 +0000

    V1.1 Update - Subflows

--- node-red-library-simplify.js
+++ node-red-library-simplify.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const infile = process.argv[2];
 if (!infile) { console.error('Provide input file'); process.exit(1); }
-const backup = infile + '(' + new Date().toJSON().slice(0,19) + ')' + '.bak';
+const backup = infile + '(' + new Date().toJSON().slice(0,19) + ').bak';
 const data = JSON.parse(fs.readFileSync(infile));
 const map = new Map(); let n = 1;
 
@@ -21,10 +21,32 @@ if (!Array.isArray(data)){
 data.forEach(node=>{
   if (node.id) node.id = getId(node.id);
   if (node.z) node.z = getId(node.z);
+  if (node.type && node.type.startsWith('subflow:')) {
+    const oldSubflowId = node.type.slice('subflow:'.length);
+    node.type = 'subflow:'+getId(oldSubflowId);
+  }
+  if (Array.isArray(node.in)) {
+    node.in.forEach(input => {
+      if (Array.isArray(input.wires)) {
+        input.wires = input.wires.map(wire =>
+          ({ ...wire, id: getId(wire.id) })
+        );
+      }
+    });
+  }
+  if (Array.isArray(node.out)) {
+    node.out.forEach(output => {
+      if (Array.isArray(output.wires)) {
+        output.wires = output.wires.map(wire =>
+          ({ ...wire, id: getId(wire.id) })
+        );
+      }
+    });
+  }
   if (node.wires) node.wires = node.wires.map(arr => arr.map(id => getId(id)));
-  if (node.type == "tab") node.id = path.parse(infile).name;
-  if (node.type == "MSSQL-CN") node.id = "mssqlcn0";
-  if (node.mssqlCN) node.mssqlCN = "mssqlcn0";
+  if (node.type == 'tab') node.id = path.parse(infile).name;
+  if (node.type == 'MSSQL-CN') node.id = 'mssqlcn0';
+  if (node.mssqlCN) node.mssqlCN = 'mssqlcn0';
 });
 
 fs.copyFileSync(infile, backup); // create backup
